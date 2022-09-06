6f9cb3f8c29c986fd6790f262bddfa5d7e25f83d
Masaya Tk <844196@users.noreply.github.com>
2022-09-06 10:59:07 +0000

    :bricks: Create VPC

--- infra/lib/arkord-stack.ts
+++ infra/lib/arkord-stack.ts
@@ -1,8 +1,11 @@
-import * as cdk from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
-export class ArkordStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ArkordStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new Vpc(this, "Vpc");
   }
 }
